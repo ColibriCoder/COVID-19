39810a8c7a980d2c41316c96107bab76b72279b2
ColibriCoder <34769235+ColibriCoder@users.noreply.github.com>
2020-03-24 16:16:24 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,27 +1,26 @@
 <?  include_once 'includes/header.php'; ?>
-        <form id="filter">
-            <label for="countries">Country/Region:</label>
-            <select id="countries">
-                <option value="0">-- SELECT --</option>
-                <? 
-                    foreach ($locations as $key => $location) {
-                        echo '<option value="' . $key . '">' . $location .'</option>';
-                    }
-                ?>
-            </select>
+	<form id="filter">
+	    <label for="countries">Country/Region:</label>
+	    <select id="countries">
+		<option value="0">-- SELECT --</option>
+		<? 
+		    foreach ($locations as $key => $location) {
+			echo '<option value="' . $key . '">' . $location .'</option>';
+		    }
+		?>
+	    </select>
 
-            <br /><br />
-            <label for="date_from">From:</label>
-            <input type="date" id="date-from" name="date_from">
+	    <br /><br />
+	    <label for="date_from">From:</label>
+	    <input type="date" id="date-from" name="date_from">
 
-            <label for="date_to">To:</label>
-            <input type="date" id="date-to" name="date_to">
-            <br /><br />
-            <input id="filter-submit" type="submit" name="" value="Filter" />
-            <br /><br />
-        </form>
-		
-        <div id="infections-table"></div>
-        
+	    <label for="date_to">To:</label>
+	    <input type="date" id="date-to" name="date_to">
+	    <br /><br />
+	    <input id="filter-submit" type="submit" name="" value="Filter" />
+	    <br /><br />
+	</form>
+
+	<div id="infections-table"></div>  
 <?  include_once 'includes/footer.php'; ?>
  
